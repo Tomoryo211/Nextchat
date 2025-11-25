ef67330a733a99f2e7be14adebce22194a0017a9
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-11-25 23:57:53 +0900

    コンポーネント(Input)value追加

--- app/components/Input/page.tsx
+++ app/components/Input/page.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import style from "./Input.module.scss";
 return(
     <div className={style.inputArea}>
-
+        <input 
+            type="text"
+            value={input} />
     </div>
 )
