8ea460e46ab603eb1d6b977ff5cd0e6cc051ab93
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-11-26 23:59:14 +0900

    コンポーネントを渡す

--- app/components/Input/page.tsx
+++ app/components/Input/page.tsx
@@ -1,6 +1,15 @@
 "use client";
 import { useState } from "react";
 import style from "./Input.module.scss";
+
+export default function MessageInput({ onSend }: { onSend: (text: string) => void }) {
+    const [input, setInput] = useState("");
+
+const handleSend = () => {
+    if (!input.trim()) return;
+    onSend(input);
+    setInput("");
+};
 return(
     <div className={style.inputArea}>
         <input 
