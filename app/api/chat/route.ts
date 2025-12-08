ee83e14e4f249079aba3d5864a480d8fd65153f6
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-12-08 10:01:49 +0900

    見返してもわかるコメント作成

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,20 +1,22 @@
 import { NextResponse } from "next/server";
 import { genAI } from "@/lib/genAI";
 
+// メッセージが送信された時にこの関数が動く
 export async function POST(req: Request) {
     try {
+        // 「message」に送信されたメッセージを取り出す
         const { message } = await req.json();
-
+        // geminiAIのモデルを探している
     const model = genAI.getGenerativeModel({
         model: "gemini-2.0-flash", 
     });
-
+    // 「message」をAIに「result」にAIからの返信が来る
     const result = await model.generateContent(message);
-
+    // replyにAIから来た返事を入れて「reply」で表示
     return NextResponse.json({
         reply: result.response.text(),
     });
-
+    // APIkeyがない時にこのエラーが起こる
     } catch (error: any) {
     console.error("API ERROR:", error);
     return NextResponse.json(
--- lib/genAI.ts
+++ lib/genAI.ts
@@ -1,5 +1,5 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-
+// env.localにAPIkeyがかるかチェックしている
 if (!process.env.GEMINI_API_KEY) {
   throw new Error("GEMINI_API_KEY が設定されていません");
 }
