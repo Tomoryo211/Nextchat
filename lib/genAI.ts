0e32948aff45afa39784808f62ec7e4f7538d84e
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-12-08 09:09:14 +0900

    文字変更

--- lib/genAI.ts
+++ lib/genAI.ts
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 if (!process.env.GEMINI_API_KEY) {
-  throw new Error("🚨 GEMINI_API_KEY が設定されていません");
+  throw new Error("GEMINI_API_KEY が設定されていません");
 }
 
 export const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
